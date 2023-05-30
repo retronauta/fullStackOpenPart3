823157afe91aa42fa859bc00a520b6d422bea877
Limbert Lino <limbertlino@gmail.com>
2023-05-30 14:06:41 -0400

    Done exercise 3.7 phonebook backend

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const PORT = 3001;
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let list = [
   {
